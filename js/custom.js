1967324d2b7acbdb7b3da7711f1177ab839bf7ec
PhyleMe <74562200+PhyleMe@users.noreply.github.com>
2021-05-20 17:20:21 -0400

    contact form work

--- js/custom.js
+++ js/custom.js
@@ -93,15 +93,10 @@ $(function(){
     });
 
 
-  // HOME BACKGROUND SLIDESHOW
-  $(function(){
-    jQuery(document).ready(function() {
-    $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-        ],  {duration: 2000, fade: 750});
-    });
-  })
 
+
+
+
+  
 });
 
